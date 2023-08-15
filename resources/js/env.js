b23976e16bd9acca98b61db447b0a594823c2b9d
thanhnt6 <thanhnt6@rikkeisoft.com>
2023-08-15 15:56:01 +0700

    release

--- resources/js/env.js
+++ resources/js/env.js
@@ -2,7 +2,10 @@ const env = {
     // api: import.meta.env.VITE_API_URL,
     // s3Url: import.meta.env.VITE_AWS_URL,
 
-    api: 'http://localhost:8000/api/v1/',
+    // api: 'http://localhost:8000/api/v1/',
+    // s3Url: 'https://marketing-courses-stg.s3.ap-southeast-1.amazonaws.com/',
+
+    api: 'http://ioiddihn.nethost-4011.000web.xyz/api/v1/',
     s3Url: 'https://marketing-courses-stg.s3.ap-southeast-1.amazonaws.com/',
 }
 
