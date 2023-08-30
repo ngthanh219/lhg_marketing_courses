d91f890ec5a44a7d1a67235662ce03b19e2dc9ea
thanhnt6 <thanhnt6@rikkeisoft.com>
2023-08-30 11:38:15 +0700

    deploy

--- resources/js/env.js
+++ resources/js/env.js
@@ -5,7 +5,7 @@ const env = {
     // api: 'http://localhost:8000/api/v1/',
     // s3Url: 'https://marketing-courses-stg.s3.ap-southeast-1.amazonaws.com/',
 
-    api: 'http://ioiddihn.nethost-4011.000web.xyz/api/v1/',
+    api: 'https://kinhdoanhthucchien.edu.vn/api/v1/',
     s3Url: 'https://marketing-courses-stg.s3.ap-southeast-1.amazonaws.com/',
 }
 
