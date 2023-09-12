889c889263d96988960cd13f2cbec7f163a65da4
thanhnt6 <thanhnt6@rikkeisoft.com>
2023-09-12 10:00:04 +0700

    update

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -14,7 +14,13 @@ class RegisterRequest extends FormRequest
     public function rules()
     {
         return [
-            'email' => 'required|email|max:255',
+            'name' => 'required|max:255',
+            'email' => [
+                'required',
+                'email',
+                'max:255',
+                'regex:/^[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/'
+            ],
             'password' => 'required|min:6|max:25',
             'phone' => 'required|regex:/(0)[0-9]{9}/|max:10',
             "verification_code" => 'required|regex:/[0-9]{5}/|max:5',
