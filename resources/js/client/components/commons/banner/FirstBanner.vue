8b13e9277b9cbb29a4284151c15112d91dd9085c
ngthanh219 <nguyentienthanh9291@gmail.com>
2023-11-02 20:04:36 +0700

    dev

--- resources/js/client/components/commons/banner/FirstBanner.vue
+++ resources/js/client/components/commons/banner/FirstBanner.vue
@@ -1,18 +1,5 @@
 <template>
-    <!-- <div class="banner"
-        v-bind:style="
-            'background: url(' + $env.s3Url + 'banners/banner.png);'
-        "
-    >
-        <div class="overlay"></div>
-        <div class="b-content">
-            <h2>NGUYỄN CẢNH CHUYÊN GIA MARKETING ĐA KÊNH</h2>
-            <span>Lớp Học Online 3 Ngày Qua Zoom</span>
-            <a href="#" class="btn btn-danger">Nhận vé miễn phí</a>
-        </div>
-    </div> -->
-
-    <div class="banner">
+    <div class="banner-image">
         <img :src="$env.s3Url + 'banners/banner.png'">
     </div>
 </template>
@@ -21,7 +8,18 @@
     export default {
         name: 'FirstBanner',
         mounted() {
-            
+            // <div class="banner"
+            //     v-bind:style="
+            //         'background: url(' + $env.s3Url + 'banners/banner.png);'
+            //     "
+            // >
+            //     <div class="overlay"></div>
+            //     <div class="b-content">
+            //         <h2>NGUYỄN CẢNH CHUYÊN GIA MARKETING ĐA KÊNH</h2>
+            //         <span>Lớp Học Online 3 Ngày Qua Zoom</span>
+            //         <a href="#" class="btn btn-danger">Nhận vé miễn phí</a>
+            //     </div>
+            // </div>
         },
     }
 </script>
