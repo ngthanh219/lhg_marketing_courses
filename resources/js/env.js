87f63a957187e5d9f11272c7d7c1717c6fc6b904
thanhnt6 <thanhnt6@rikkeisoft.com>
2024-01-31 13:59:24 +0700

    develop

--- resources/js/env.js
+++ resources/js/env.js
@@ -1,6 +1,6 @@
 const env = {
     api: 'http://localhost:8000/api/v1/',
-    s3Url: 'https://kdtc.s3.ap-southeast-1.amazonaws.com/',
+    s3Url: 'https://pmtkdtc.s3.ap-southeast-1.amazonaws.com/',
 }
 
 export default env;
